a47af10168566c5ed264bc763bcd8fcdc3e1d598
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2020-07-08 18:36:20 -0700

    implemented rest of Linked List java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -53,20 +53,105 @@ public class LinkedList<T> {
         }
     }
 
-    // getHead (to iterate)
+    // @desc    getHead (to iterate)
+    // @usage   Node<T> iterator = linkedList.getIterator();
+    //          iterator.getNext(); iterator.getData(); ...etc 
     public Node<T> getIterator() {
         return this.head;
+    }
+
+    // @desc    get data of given position
+    public T getDataByPos(int pos) {
+        Node<T> nodeAtPos = nthNode(pos);
+        if (nodeAtPos == null) {
+            throw new IndexOutOfBoundsException("Out Of Range");
+        }
+        return nodeAtPos.data;
+    }
+
+    // @desc    insert the given value in the start of list
+    public void insertAtStart(T data) {
+        Node<T> newNode = new Node<>(data, this.head);
+        this.head = newNode;
+    }
+
+    // @desc    append the given value at the end of list
+    public void append(T data) {
+        Node<T> newNode = new Node<>(data);
+        // if list is empty
+        if (this.head == null) {
+            this.head = newNode;
+            return;
+        }
+        Node<T> it_ptr = this.head;
+        while (it_ptr.next != null) {
+            it_ptr = it_ptr.next;
+        }
+        it_ptr.next = newNode;
+    }
+
+    // @desc    insert at given position
+    public void insert(T data, int pos) {
+        if (pos == 0) {
+            insertAtStart(data);
+            return;
+        }
+        Node<T> prevPtr = nthNode(pos-1);
+        // if list is not long enough, simply append to end
+        if (prevPtr == null) {
+            append(data);
+            return;
+        }
+        Node<T> newNode = new Node<>(data, prevPtr.next);
+        prevPtr.next = newNode;
+    }
+
+    // @desc    remove a node from a list at given position
+    public void remove(int pos) {
+        Node<T> nodeToDelete = nthNode(pos);
+        // if position is not within linked list range
+        if (nodeToDelete == null) {
+            throw new IndexOutOfBoundsException("Out Of Range");
+        }
+        // if removing head
+        if (pos == 0) {
+            this.head = this.head.next;
+        } else {
+            Node<T> prevNode = nthNode(pos-1);
+            prevNode.next = nodeToDelete.next;
+        }
+    }
+
+    // @desc    clear the list
+    public void clear() {
+        this.head = null;
     }
 
     @Override
     public String toString() {
         Node<T> it_ptr = this.head;
-        StringBuilder TO_STRING = new StringBuilder();
+        StringBuilder TO_STRING = new StringBuilder("{");
         while(it_ptr != null) {
-            TO_STRING.append(it_ptr.data.toString() + " ");
+            TO_STRING.append(it_ptr.data.toString());
+            if (it_ptr.next != null) {
+                TO_STRING.append(" -> ");
+            }
             it_ptr = it_ptr.next;
         }
+        TO_STRING.append("}");
         return TO_STRING.toString();
+    }
+
+    // HELPER function to iterate to Nth position
+    private Node<T> nthNode(int pos) {
+        Node<T> it_ptr = this.head;
+        for (int i = 0; i < pos; i++) {
+            if (it_ptr == null) {
+                return it_ptr;
+            }
+            it_ptr = it_ptr.next;
+        }
+        return it_ptr;
     }
 
 }
--- LinkedList/LinkedListTest.java
+++ LinkedList/LinkedListTest.java
@@ -5,13 +5,45 @@ import LinkedList.LinkedList.Node;
 public class LinkedListTest {
     
     public static void main(String[] args) {
+        // array constructor test
         Integer[] arr = {1, 2, 3, 4, 5};
         LinkedList<Integer> ll = new LinkedList<>(arr);
         System.out.println(ll.toString());
 
+        // iterator test
         Node<Integer> it = ll.getIterator();
         it = it.getNext();
         System.out.println(it.getData());
+
+        // get value by position test
+        System.out.println("Value at 'index' 3: " + ll.getDataByPos(3));
+
+        // insert to beginning of list test
+        ll.insertAtStart(0);
+        System.out.println(ll.toString());
+
+        // append to end of list test
+        ll.append(6);
+        System.out.println(ll.toString());
+
+        // insert at specific position test
+        ll.insert(8, 4);
+        System.out.println(ll.toString());
+
+        // remove element test
+        try {
+            ll.remove(2);
+            ll.remove(6);
+            ll.remove(0);
+            ll.remove(8);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(ll.toString());
+
+        // clear test
+        ll.clear();
+        System.out.println(ll.toString());
     }
 
 }

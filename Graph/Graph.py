7f6b00bd6c38790c81dc775e631674d409fe2247
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2020-07-12 15:35:09 -0700

    added error handling on Graph.py

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -31,6 +31,8 @@ class MatrixGraph:
         self.__numVertex = 0
 
     def addVertex(self, vertex):
+        if vertex in self.__vertices:
+            raise Exception("Given vertex already exists")
         self.__vertices.append(vertex)
         self.__numVertex += 1
         # add new vertex as new row and column of adj matrix
@@ -40,6 +42,8 @@ class MatrixGraph:
             self.__adjMatrix[i].append(0)
 
     def removeVertex(self, vertex):
+        if vertex not in self.__vertices:
+            raise Exception("Given vertex does not exist")
         vertexIndex = self.__vertices.index(vertex)
         self.__vertices.remove(vertex)
         del self.__adjMatrix[vertexIndex]
@@ -48,6 +52,10 @@ class MatrixGraph:
         self.__numVertex -= 1
 
     def addEdge(self, source, destination, weight=1):
+        if source not in self.__vertices:
+            raise Exception("Given source vertex does not exist")
+        if destination not in self.__vertices:
+            raise Exception("Given destination vertex does not exist")
         sourceIndex = self.__vertices.index(source)
         destIndex = self.__vertices.index(destination)
         if not self.__weighted:
@@ -57,6 +65,10 @@ class MatrixGraph:
             self.__adjMatrix[destIndex][sourceIndex] = weight
 
     def removeEdge(self, source, destination):
+        if source not in self.__vertices:
+            raise Exception("Given source vertex does not exist")
+        if destination not in self.__vertices:
+            raise Exception("Given destination vertex does not exist")
         sourceIndex = self.__vertices.index(source)
         destIndex = self.__vertices.index(destination)
         self.__adjMatrix[sourceIndex][destIndex] = 0
@@ -109,6 +121,10 @@ def testMatrixGraph():
     print(graph)
     graph.clearEdge()
     print(graph)
+    try:
+        graph.removeVertex('d')
+    except Exception as e:
+        print(e)
 
 
 testMatrixGraph()
@@ -124,15 +140,19 @@ class ListGraph:
         self.__directed = directed
 
     def addVertex(self, vertex):
+        if vertex in self.__vertices:
+            raise Exception("Given vertex already exists")
         self.__vertices.append(vertex)
         self.__numVertex += 1
         self.__adjList.append([])
 
     def removeVertex(self, vertex):
+        if vertex not in self.__vertices:
+            raise Exception("Given vertex does not exist")
         vertexIndex = self.__vertices.index(vertex)
-        self.__vertices = [v for v in self.__vertices if v != vertex]
+        self.__vertices.remove(vertex)
+        self.__numVertex -= 1
         del self.__adjList[vertexIndex]
-        self.__numVertex = len(self.__vertices)
         # find removed vertex from other edges
         if self.__weighted:
             for i in range(0, len(self.__adjList)):
@@ -140,17 +160,20 @@ class ListGraph:
                     adj for adj in self.__adjList[i] if adj[0] != vertex]
         else:
             for i in range(0, len(self.__adjList)):
-                self.__adjList[i] = [
-                    adj for adj in self.__adjList[i] if adj != vertex]
+                self.__adjList[i].remove(vertex)
 
     def addEdge(self, source, destination, weight=1):
+        if source not in self.__vertices:
+            raise Exception("Given source vertex does not exist")
+        if destination not in self.__vertices:
+            raise Exception("Given destination vertex does not exist")
         sourceIndex = self.__vertices.index(source)
         if not self.__weighted:
             newAdj = destination
         else:
             newAdj = (destination, weight)
         self.__adjList[sourceIndex].append(newAdj)
-
+        # if undirected add edge the other way too
         if not self.__directed:
             destIndex = self.__vertices.index(destination)
             if not self.__weighted:
@@ -160,12 +183,17 @@ class ListGraph:
             self.__adjList[destIndex].append(newAdj)
 
     def removeEdge(self, source, destination):
+        if source not in self.__vertices:
+            raise Exception("Given source vertex does not exist")
+        if destination not in self.__vertices:
+            raise Exception("Given destination vertex does not exist")
         sourceIndex = self.__vertices.index(source)
         if self.__weighted:
             self.__adjList[sourceIndex] = [
                 adj for adj in self.__adjList[sourceIndex] if adj[0] != destination]
         else:
             self.__adjList[sourceIndex].remove(destination)
+        # if undirected, remove edge from dest to src
         if not self.__directed:
             destIndex = self.__vertices.index(destination)
             if self.__weighted:
@@ -174,8 +202,12 @@ class ListGraph:
             else:
                 self.__adjList[destIndex].remove(source)
 
+    def clearEdge(self):
+        self.__adjList = [[]]*self.__numVertex
+
     # utilities
     # print out adjacency list
+
     def __str__(self):
         output = ""
         for i in range(0, self.__numVertex):
@@ -200,6 +232,8 @@ def testListGraph():
     graph.removeEdge('a', 'b')
     print(graph)
     graph.removeVertex('a')
+    print(graph)
+    graph.clearEdge()
     print(graph)
 
 
